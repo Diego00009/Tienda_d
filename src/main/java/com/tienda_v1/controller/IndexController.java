8f4ae6faf38f0c7bf0d5ceba23235ac06ea2087d
Diego00009 <123181565+Diego00009@users.noreply.github.com>
2023-04-14 12:01:52 -0500

    Add files via upload
    semana 13

--- src/main/java/com/tienda_v1/controller/IndexController.java
+++ src/main/java/com/tienda_v1/controller/IndexController.java
@@ -2,6 +2,8 @@ package com.tienda_v1.controller;
 
 
 
+import com.tienda_v1.services.ArticuloService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +11,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
     
-  
+     @Autowired
+    private ArticuloService articuloService;
+     
     @GetMapping("/")
     public String inicio(Model model) {
+        var articulos=articuloService.getArticulos(false);
+        model.addAttribute("articulos",articulos);
         return "index";
     }
 }
